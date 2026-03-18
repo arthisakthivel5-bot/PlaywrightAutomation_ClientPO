c63a0346df7af8b0f507ba39c69728f47c9c56ad
Arthi Sakthivel <arthisakthivel5@gmail.com>
2026-03-18 10:26:57 +0530

    modified loginHelper.js

--- utils/loginHelper.js
+++ utils/loginHelper.js
@@ -5,7 +5,7 @@ async function loginHub(page) {
 
     await page.goto(process.env.BASE_URL);
     await page.getByPlaceholder("you@email.com").fill(process.env.USERNAME);
-    await page.getByLabel("Password").fill(process.env.USERNAME);
+    await page.getByLabel("Password").fill(process.env.PASSWORD);
     await page.locator("#login-btn").click();
     await expect(page.getByRole('link', { name: 'Browse Events →' })).toBeVisible();
 
