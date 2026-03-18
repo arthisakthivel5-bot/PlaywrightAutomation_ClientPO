a73200250847208d4e0a7f1be0143a579798f419
Arthi Sakthivel <arthisakthivel5@gmail.com>
2026-03-18 10:55:02 +0530

    modtified config file

--- playwright.config.js
+++ playwright.config.js
@@ -8,11 +8,13 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
   use: {
     trace: 'retain-on-failure',
-    screenshot:'only-on-failure',
-    video:'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     baseURL: process.env.BASE_URL_CLIENT || process.env.BASE_URL
   },
 
